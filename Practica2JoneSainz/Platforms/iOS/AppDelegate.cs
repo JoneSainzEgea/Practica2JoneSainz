50f12a430aed3ac2ab95afca931b44cf405da873
Jonetxu10 <jonesainzegea@gmail.com>
2026-04-19 11:13:39 +0200

    Agregar archivos de proyecto.

---
+++ Practica2JoneSainz/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Practica2JoneSainz
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
