379306e08ebfa51ab315058250fb2061a6bf37d3
Jonetxu10 <jonesainzegea@gmail.com>
2026-04-22 01:01:27 +0200

    BasicConfiguration

--- Practica2JoneSainz/Configuracion.xaml.cs
+++ Practica2JoneSainz/Configuracion.xaml.cs
@@ -6,4 +6,56 @@ public partial class Configuracion : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void OnThemeClicked(object sender, EventArgs e)
+    {
+        if (Application.Current.UserAppTheme == AppTheme.Dark)
+        {
+            Application.Current.UserAppTheme = AppTheme.Light;
+        }
+        else
+        {
+            Application.Current.UserAppTheme = AppTheme.Dark;
+        }
+    }
+
+    private void OnFontSizeClicked(object sender, EventArgs e)
+    {
+        double currentSize = (double)Application.Current.Resources["globalFontSize"];
+
+        if (currentSize == 16) // De Normal a Grande
+            Application.Current.Resources["globalFontSize"] = 24.0;
+        else if (currentSize == 24) // De Grande a Pequeño
+            Application.Current.Resources["globalFontSize"] = 12.0;
+        else // De Pequeño a Normal
+            Application.Current.Resources["globalFontSize"] = 16.0;
+    }
+
+    private void OnFontFamilyClicked(object sender, EventArgs e)
+    {
+        if (Application.Current.Resources["globalFontFamily"].ToString() == "OpenSansRegular")
+        {
+            Application.Current.Resources["globalFontFamily"] = "Courier New";
+        }
+        else
+        {
+            Application.Current.Resources["globalFontFamily"] = "OpenSansRegular";
+        }
+    }
+
+    private void OnColorClicked(object sender, EventArgs e)
+    {
+        Color currentColor = (Color)Application.Current.Resources["globalTextColor"];
+
+        if (currentColor == Colors.Black || currentColor == Colors.White)
+        {
+            Application.Current.Resources["globalTextColor"] = Colors.DarkGreen;
+        }
+        else
+        {
+            // Devolvemos el color según el tema actual
+            Application.Current.Resources["globalTextColor"] =
+                Application.Current.UserAppTheme == AppTheme.Dark ? Colors.White : Colors.Black;
+        }
+    }
 }
