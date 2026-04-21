1e974775b7956e9ca9813140f0d162e74e7dab65
Jonetxu10 <jonesainzegea@gmail.com>
2026-04-21 13:23:31 +0200

    PaginaHome

--- Practica2JoneSainz/HomePage.xaml.cs
+++ Practica2JoneSainz/HomePage.xaml.cs
@@ -1,3 +1,6 @@
+using Plugin.Fingerprint;
+using Plugin.Fingerprint.Abstractions;
+
 namespace Practica2JoneSainz;
 
 public partial class HomePage : ContentPage
@@ -6,4 +9,14 @@ public partial class HomePage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void OnGaleriaClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new Galeria());
+    }
+
+    private async void OnConfigClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new Configuracion());
+    }
 }
